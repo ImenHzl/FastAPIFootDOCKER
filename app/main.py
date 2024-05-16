4ff2f494cf2883463cb8f44cb1c267ae4b3664a9
unknown <imenhzl14@gmail.com>
2024-05-16 15:08:44 +0200

    👌 IMPROVE: creation de API depuis un fichier Json

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import json
+
+app= FastAPI()
+
+# Charger les données JSON depuis le fichier
+with open("data/dataTeams.json", "r") as file:
+    data = json.load(file)
+@app.get("/")
+def read_root():
+    return data
